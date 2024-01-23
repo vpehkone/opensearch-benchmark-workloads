b4ecf06e814e0b96b7228406654fe4f40207f35e
Vesa Pehkonen <vesa.pehkonen@intel.com>
2024-01-22 16:00:55 -0800

    Updated and added random queries.

--- marco/workload.py
+++ marco/workload.py
@@ -1,5 +1,49 @@
-async def delete_snapshot(opensearch, params):
-    await opensearch.snapshot.delete(repository=params["repository"], snapshot=params["snapshot"])
+import random
+import os
+
+def random_query(workload, params, **kwargs):
+    # choose a suitable index: if there is only one defined for this workload                                                                                                                                                                                                             
+    # choose that one, but let the user always override index and type.                                                                                                                                                                                                                   
+    if len(workload.indices) == 1:
+        default_index = workload.indices[0].name
+        if len(workload.indices[0].types) == 1:
+            default_type = workload.indices[0].types[0].name
+        else:
+            default_type = None
+    else:
+        default_index = "_all"
+        default_type = None
+
+    index_name = params.get("index", default_index)
+    type_name = params.get("type", default_type)
+    model_id = params.get("model_id", None)
+    script_dir=os.path.dirname(os.path.realpath(__file__))
+    random_query = random.choice(open(script_dir + '/queries.tmp').readlines())
+
+    return {
+        "body": {
+            "track_total_hits": True,
+            "_source": {
+                "excludes": [
+                    "passage_embedding"
+                ]
+            },
+            "query": {
+                "neural": {
+                    "passage_embedding": {
+                        "query_text": random_query,
+                        "model_id": model_id,
+                        "k": 5
+                    }
+                }
+            }
+        },
+        "index": index_name,
+        "type": type_name,
+        "cache": params.get("cache", False)
+    }
+
 
 def register(registry):
-    registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
+    registry.register_param_source("semantic-search-random-source", random_query)
+
